549b6a302a45e9943d564e4bb8a1676fcb88d98c
DaysiMassielka <54219496+DaysiMassielka@users.noreply.github.com>
2025-06-18 21:22:22 +0000

    Reenvio generador de nombres de dominio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
+  let pronouns = ['the', 'our'];
+  let adjs = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+  let exts = ['.com', '.es'];
+
+  let dominios = [];
+
+  pronouns.map(pronoun => {
+    adjs.map(adj => {
+      nouns.map(noun => {
+        exts.map(ext => {
+          dominios.push(pronoun, adj, noun, ext);
+        })
+      })
+    })
+})
+
+dominios.map(dominio => {
+  console.log(dominio);
+})
 };
